d908ae80fc724f4a3a42a256d80cfce4960af88c
Caldremch <caldremch@163.com>
2020-08-25 16:58:18 +0800

    add kotlin plugin

--- bintray-with-maven-publish.gradle.kts
+++ bintray-with-maven-publish.gradle.kts
@@ -9,6 +9,7 @@ buildscript {
     dependencies {
         classpath("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.5")
         classpath("com.android.tools.build:gradle:4.0.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.0")
     }
 }
 
